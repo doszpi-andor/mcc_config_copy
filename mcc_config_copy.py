f17d306b6f4e667e5b7ae37e6da1f5fc8b1b3d94
Andor Doszpod <doszpod.andor@gmail.com>
2023-12-09 10:04:33 +0100

    create read_mplab_project_path metod

--- mcc_config_copy.py
+++ mcc_config_copy.py
@@ -1,4 +1,4 @@
-from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import askopenfilename, askdirectory
 from tkinter.messagebox import showerror
 
 
@@ -6,8 +6,15 @@ def read_mcc_file_path():
     mcc_file = askopenfilename(filetypes=[("MCC file", "*.mc3")], initialdir='.')
 
     if mcc_file.split('.')[-1] != 'mc3':
-        showerror('Error', 'File is not MCC file')
         raise FileExistsError
 
     return mcc_file
 
+
+def read_mplab_project_path():
+    project = askdirectory()
+
+    if project != '' and project[-1] == 'X':
+        return project
+    else:
+        raise 
