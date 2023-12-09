edd0e80ac9977601db73b711d610784856af2f81
Andor Doszpod <doszpod.andor@gmail.com>
2023-12-09 10:05:27 +0100

    create Exception

--- mcc_config_copy.py
+++ mcc_config_copy.py
@@ -2,11 +2,25 @@ from tkinter.filedialog import askopenfilename, askdirectory
 from tkinter.messagebox import showerror
 
 
+class OpenMccFileException(Exception):
+    """
+    It is not mcc file
+    """
+    pass
+
+
+class OpenMplabProjectException(Exception):
+    """
+    It is not mplab project folder
+    """
+    pass
+
+
 def read_mcc_file_path():
     mcc_file = askopenfilename(filetypes=[("MCC file", "*.mc3")], initialdir='.')
 
     if mcc_file.split('.')[-1] != 'mc3':
-        raise FileExistsError
+        raise OpenMccFileException
 
     return mcc_file
 
@@ -17,4 +31,4 @@ def read_mplab_project_path():
     if project != '' and project[-1] == 'X':
         return project
     else:
-        raise 
+        raise OpenMplabProjectException
